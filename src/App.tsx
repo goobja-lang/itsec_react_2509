b983d83dee81efc77232ec55d7a742f7403d8c45
goobja-lang <goobja@naver.com>
2025-10-29 10:13:50 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./component/Header";
 import Home from "./page/Home";
 import Footer from "./component/Footer";
+import MemoDetail from "./page/MemoDetail";
+import MemoUpsert from "./page/MemoUpsert";
+import LoginPage from "./page/LoginPage";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/memo_detail" element={<MemoDetail />} />
+          <Route path="/memo_upsert" element={<MemoUpsert />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/types/global_types.ts
+++ src/types/global_types.ts
@@ -5,3 +5,19 @@ export interface Memotype {
   id: number;
   title: string;
 }
+
+export interface UserInfoType {
+  id: number;
+  username: string;
+  password: string;
+  createdDt: string;
+  updatedDt: string;
+  realName: string;
+  profileUrl: string;
+  uid: string;
+  email: string;
+  displayName: string;
+  providerId: string;
+  metadata: string;
+  token: string;
+}
